1e0dab681b05ab69d14e5ea396678a77696a5cda
bytes <anongecko@tutamail.com>
2024-12-11 17:05:36 -0500

    Minor fix

--- lua/telescope/_extensions/keywhiz/themes/init.lua
+++ lua/telescope/_extensions/keywhiz/themes/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.themes = {
-  catppuccin = require("telescope._extensions/keywhiz/themes/catppuccin"),
   spacedust = require("telescope._extensions/keywhiz/themes/spacedust"),
   github = require("telescope._extensions/keywhiz/themes/github"),
   nord = require("telescope._extensions/keywhiz/themes/nord"),
